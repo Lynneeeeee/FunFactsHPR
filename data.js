4d48037d6cd85efdb1325acbee58fe387d927c88
Yingxue <yingxue0323@gmail.com>
2024-04-05 09:15:25 +1100

    finished front-end design

---
+++ data.js
@@ -0,0 +1,43 @@
+const CATEGORES = [
+    {name: 'General', color: '#3b82f6'},
+    {name: 'Business', color: '#16a34a'},
+    {name: 'Dynamics', color: '#ef4444'},
+    {name: 'Structures', color: '#eab308'},
+    {name: 'Porpulsion', color: '#db2777'},
+    {name: 'Control', color: '#14b8a6'},
+    {name: 'FlightSys', color: '#f97316'},
+    {name: 'Payload', color: '#8b5cf6'}
+];
+
+const initialFacts = [
+    {
+        id: 1,
+        text: 'Zenith is the name of our rocket for SAC2025',
+        source: 'https://zenith.com/',
+        category: 'General',
+        votesInteresting: 24,
+        votesMindblowing: 9,
+        votesFalse: 4,
+        createdIn: 2021,
+    },
+    {
+        id: 2,
+        text: 'We currently have 3 Alex in HPR',
+        source: 'https://teams.com/',
+        category: 'Dynamics',
+        votesInteresting: 11,
+        votesMindblowing: 2,
+        votesFalse: 0,
+        createdIn: 2019,
+    },
+    {
+        id: 3,
+        text: 'Aether aims to 30,000 ft',
+        source: 'https://www.monashhpr.com/',
+        category: 'Structures',
+        votesInteresting: 8,
+        votesMindblowing: 3,
+        votesFalse: 1,
+        createdIn: 2015,
+    },
+]
